0e97eab49a2ae4b1eceda212ecd1214ef2ec4e9d
chen923073366 <chenguoliang@mychery.com>
2020-02-26 13:53:15 +0800

    log

---
+++ src/main/java/com/chery/peer2/Peer2Application.java
@@ -0,0 +1,13 @@
+package com.chery.peer2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Peer2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Peer2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/chery/peer2/Peer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chery.peer2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Peer2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
